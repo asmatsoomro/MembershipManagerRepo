5576b0c0a66ff8444c7d2535c8685954bdbd453a
Asmat Soomro <soomro.asmat@gmail.com>
2020-04-11 15:46:40 -0400

    Build specs added for AWS code pipeline

--- src/main/java/com/membermanager/core/Main.java
+++ src/main/java/com/membermanager/core/Main.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableAutoConfiguration
 public class Main {
 
 	public static void main(String[] args) {

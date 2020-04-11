a3d219cec6f2d17781d5f1cdd8bbf90abb9f1306
Asmat Soomro <soomro.asmat@gmail.com>
2020-04-11 16:46:51 -0400

    Packaging converted to war

--- src/main/java/com/membermanager/core/Main.java
+++ src/main/java/com/membermanager/core/Main.java
@@ -1,15 +1,21 @@
 package com.membermanager.core;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Main.class);
 	}
 }
